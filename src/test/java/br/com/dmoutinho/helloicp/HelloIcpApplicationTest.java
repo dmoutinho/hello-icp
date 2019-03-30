6d94fd38951516e9dd3b1013cec01a54093d51a3
dmoutinho <dmoutinho@gmail.com>
2019-03-30 16:08:06 -0300

    Adding test

---
+++ src/test/java/br/com/dmoutinho/helloicp/HelloIcpApplicationTest.java
@@ -0,0 +1,29 @@
+package br.com.dmoutinho.helloicp;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HelloIcpApplicationTest {
+	
+	@Test
+	public void whenGetVersion() {
+	
+		// given
+		Version version = new Version("SNAPSHOT");
+		
+		// when
+		String v = "1.0.0";
+		//version.setVersion(v);
+
+		// then
+		assertTrue(version.getVersion().equals(v));		
+	
+	}
+
+}
